f228fae72797f9351879abe91ccb6281e2d0d26d
Dlucasz027 <matheuslucas.027@gmail.com~>
2026-02-06 20:13:02 -0300

    feat: add dynamic task insertion and DOM manipulation

--- noteS/listTasks.js
+++ noteS/listTasks.js
@@ -1,9 +1,20 @@
 let makeTask= (event) => {
+
     event.preventDefault()
+
+    let list = document.querySelector('[data-list]')
     let input = document.querySelector('[data-form-input]')
     let values = input.value
-    console.log(values)
+
+    let task = document.createElement('li')
+    task.classList.add('task')
+    let content = `<p class="content">${values}<p>`
+    
+    task.innerHTML = content
+    list.appendChild(task)
+
     input.value = " "
+
 }
 
 let newTask = document.querySelector('[data-form-button]')
