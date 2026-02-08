02888f543d3acfeda1fee760b4a8ad799a69d255
Dlucasz027 <matheuslucas.027@gmail.com~>
2026-02-08 10:36:11 -0300

    feat: add done button to mark tasks as completed and use IIFE

--- noteS/listTasks.js
+++ noteS/listTasks.js
@@ -1,4 +1,5 @@
-let makeTask= (event) => {
+( () => {
+    let makeTask= (event) => {
 
     event.preventDefault()
 
@@ -9,10 +10,11 @@ let makeTask= (event) => {
     let task = document.createElement('li')
     task.classList.add('task')
     let content = `<p class="content">${values}<p>`
-    
-    task.innerHTML = content
-    list.appendChild(task)
 
+    task.innerHTML = content
+    
+    task.appendChild(DoneButton())
+    list.appendChild(task)
     input.value = " "
 
 }
@@ -20,3 +22,22 @@ let makeTask= (event) => {
 let newTask = document.querySelector('[data-form-button]')
 
 newTask.addEventListener('click', makeTask)
+
+let DoneButton = () => {
+    let doneButton = document.createElement('button')
+
+    doneButton.classList.add('check-button')
+    doneButton.innerText = 'Done'
+
+    doneButton.addEventListener('click', doneTask)
+    return doneButton
+}
+
+let doneTask = (event ) => {
+    let doneButton = event.target
+
+    let taskCompleted = doneButton.parentElement
+
+    taskCompleted.classList.toggle('done')
+}
+})()
