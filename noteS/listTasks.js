e97dffaaf3d9838f28e9d6711cf9764bbcd0224f
Dlucasz027 <matheuslucas.027@gmail.com~>
2026-02-09 14:30:35 -0300

    feat: implement remove and complete task buttons

--- noteS/listTasks.js
+++ noteS/listTasks.js
@@ -1,26 +1,25 @@
 ( () => {
+    
     let makeTask= (event) => {
 
     event.preventDefault()
-
     let list = document.querySelector('[data-list]')
     let input = document.querySelector('[data-form-input]')
     let values = input.value
-
     let task = document.createElement('li')
+
     task.classList.add('task')
     let content = `<p class="content">${values}<p>`
 
     task.innerHTML = content
     
     task.appendChild(DoneButton())
+    task.appendChild(RemoveButton())
     list.appendChild(task)
     input.value = " "
-
 }
 
 let newTask = document.querySelector('[data-form-button]')
-
 newTask.addEventListener('click', makeTask)
 
 let DoneButton = () => {
@@ -28,16 +27,30 @@ let DoneButton = () => {
 
     doneButton.classList.add('check-button')
     doneButton.innerText = 'Done'
-
     doneButton.addEventListener('click', doneTask)
     return doneButton
 }
 
 let doneTask = (event ) => {
     let doneButton = event.target
-
     let taskCompleted = doneButton.parentElement
 
     taskCompleted.classList.toggle('done')
+}
+
+let RemoveButton = () => {
+    let removeButton = document.createElement('button')
+    removeButton.innerText = 'Remove'
+
+    removeButton.addEventListener('click', removeTask)
+    return removeButton
+}
+
+let removeTask = (event) => {
+    let removeButton = event.target
+    let taskcompleted = removeButton.parentElement
+
+    taskcompleted.remove()
+    return removeButton
 }
 })()
